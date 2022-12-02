9f9cc797ab1447794618732f2a4d2ef3c6a1f758
UC-DRO <daniel.rossa@unicredit.de>
2022-12-02 17:16:01 +0100

    add implementation of guessing game

---
+++ guessinggame.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+the_truth=$(ls -l | grep ^- | wc -l)
+
+function is_numeric() {
+	if [[ $@ =~ ^[0-9]+$ ]]
+	then
+		true
+	else
+		false
+	fi    
+}
+
+function is_correct() {
+	if [[ $1 -eq $the_truth ]]
+	then
+		true
+	else
+		false
+	fi
+}
+
+function give_hint() {
+	if [[ $1 -lt $the_truth ]]
+	then
+		echo $"$1 is too low, try again!"
+	else
+		echo $"$1 is too high, try again!"
+	fi    
+}
+
+echo "Let's play a game: How many files are in the current directory?"
+while true
+do
+	read guess
+
+	is_numeric $guess
+	if	[[ $? -eq 1 ]]
+	then
+		echo "That is not a valid numeric guess :( Try again!"
+		continue
+	fi
+
+	is_correct $guess
+	if [[ $? -eq 0 ]]
+	then
+		echo "Wow, you won the game, awesome!"
+		break
+	fi
+
+	give_hint $guess
+done
